a981963a0cc02e2e0f4dda9b5fabfd43d48a8765
vikrant <vikrant.negi74@gmail.com>
2021-05-23 15:41:54 +0530

    Update App.js

--- TeslaClone/App.js
+++ TeslaClone/App.js
@@ -1,24 +1,43 @@
 /* eslint-disable prettier/prettier */
 import React from 'react';
-import { SafeAreaView, ScrollView, StatusBar, StyleSheet, Text, View} from 'react-native';
+import { SafeAreaView, ScrollView, StatusBar, ImageBackground, StyleSheet, Text, View} from 'react-native';
+import VehicleComponent from './src/components/VehicleComponent';
 
 const App = () => {
   return (
     <View style={styles.AppContainer}>
-      <Text>Hello</Text>
+      <VehicleComponent />
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   AppContainer: {
-    paddingTop: 20,
     height: '100%',
     backgroundColor: 'white',
-    display: 'flex',
-    flexDirection: 'row',
-    justifyContent: 'center',
+    flex: 1,
+  },
+  VehicleContainer: {
+    width: '100%',
+    height: '100%',
+  },
+  VehicleImage: {
+    width: '100%',
+    height: '100%',
+    resizeMode: 'cover',
+    position: 'absolute',
+  },
+  VehicleDescription: {
+    marginTop: '30%',
     alignItems: 'center',
+  },
+  VehicleModel: {
+    fontSize: 40,
+    fontWeight: '600',
+  },
+  VehiclePrice: {
+    fontSize: 16,
+    color: '#5c5e62',
   },
 });
 
